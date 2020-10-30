4845628274f609e1f5baad820859ce45d7ab3b77
Pratik <sonulenovob50@gmail.com>
2020-10-30 22:28:42 +0530

    Add

---
+++ speciB.py
@@ -0,0 +1,16 @@
+# Part of special variable __name__
+#merged with 16thA,speciA
+#merged with module 16thB
+
+def add():
+    print("Addition ",__name__)
+
+def sub():
+    print("subtraction")
+
+def main():
+    add()
+    sub()
+
+if __name__ == "__main__":
+    main()
